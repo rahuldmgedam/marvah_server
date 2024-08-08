95927ba5a71cc533491d152625dff4a975e3febc
rahuldmgedam <gedamrahul19@gmail.com>
2024-08-08 14:55:59 +0530

    8th aug mukesh

--- index.js
+++ index.js
@@ -27,12 +27,12 @@ const { variationRouter } = require("./routes/petrol/variation.route")
 const { petrolProductRouter } = require("./routes/petrol/PetrolProduct.routes")
 const { ProductPetrolInvoiceFeedingRouter } = require("./routes/petrol/petrolInvoiceFeeding.routes")
 const { PetrolDecantationRouter } = require("./routes/petrol/petrolDecandation.routes")
-<<<<<<< HEAD
+
 const { Credit_Client_Router } = require("./routes/credit_client/credit_client.route")
-=======
+
 const { addOilRouter } = require("./routes/oil/addOil.route")
 const { purchaseOilRouter } = require("./routes/oil/purchaseOil.route")
->>>>>>> e9cb1e60df8403d8d1ec511b29895e0d0e450920
+
 require("dotenv").config()
 
 const app = express()
@@ -64,25 +64,22 @@ app.use("/client",clientRouter)
 
  app.use("/nozzleproductwise",nozzleproductwiseRouter)
  app.use("/fuelsales",fuelSalesRouter);
-<<<<<<< HEAD
- app.use("/variation",variationRouter)
 
-=======
  app.use("/variation",variationRouter);
 
  app.use("/addoil",addOilRouter);
->>>>>>> e9cb1e60df8403d8d1ec511b29895e0d0e450920
+
 
 // mukesh code
  app.use("/petrol",petrolProductRouter)
  app.use("/petrolInvoiceFeeding", ProductPetrolInvoiceFeedingRouter)
 app.use("/petroldecantation", PetrolDecantationRouter)
-<<<<<<< HEAD
+
 app.use("/credit_client", Credit_Client_Router)
 
-=======
+
 app.use("/purchaseoil",purchaseOilRouter)
->>>>>>> e9cb1e60df8403d8d1ec511b29895e0d0e450920
+
 
 
 
