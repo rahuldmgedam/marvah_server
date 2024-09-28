c9e88f83d1b753a060f77322bd6ce5dfae619996
rahuldmgedam <gedamrahul19@gmail.com>
2024-09-28 14:06:46 +0530

    rahul

--- models/tank.model.js
+++ models/tank.model.js
@@ -2,13 +2,17 @@ const mongoose = require("mongoose");
 
 const tankSchema = mongoose.Schema(
   {
+    date: {type:String,default:new Date},
     tankNo: { type: Number, required: true },
     product: { type: String, required: true },
     capacity: { type: Number, required: true },
     nozzels: { type: Number, required: true },
+    opStock:{ type: Number, required: true }
   },
   {
     versionKey: false,
+  },{
+    timestamps:true
   }
 );
 
