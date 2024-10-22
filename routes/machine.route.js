69cc4070f18fea49c29c6c2a7235b78f5bb794c9
rahuldmgedam <gedamrahul19@gmail.com>
2024-10-22 11:08:49 +0530

    22 oct

--- routes/machine.route.js
+++ routes/machine.route.js
@@ -1,9 +1,10 @@
 const express = require("express");
-const { createMachine, showMachine, updateMachine, deleteMachine } = require("../controller/machine.controller");
+const { createMachine, showMachine, updateMachine, deleteMachine, newMachineCreate } = require("../controller/machine.controller");
 
 const MachineRouter = express.Router();
 
 MachineRouter.post("/create",createMachine);
+MachineRouter.post("/newMachineCreate",newMachineCreate);
 MachineRouter.get("/",showMachine);
 MachineRouter.patch("/update/:machineId",updateMachine);
 MachineRouter.delete("/delete/:machineId",deleteMachine);
